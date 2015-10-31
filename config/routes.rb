20a385c807962a551443173f357ee90d422d5edf
PavithraPerera <pavithrahp@gmail.com>
2015-10-31 18:56:55 +0530

    addedmodels+votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :ministers
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
+  resources :posts do
+  member do
+    put "like", to: "posts#upvote"
+    put "dislike", to: "posts#downvote"
+  end
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
