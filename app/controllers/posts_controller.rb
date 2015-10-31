8b3269540bf1f4ba902b10f3759c5d68c643bc2f
PavithraPerera <pavithrahp@gmail.com>
2015-10-31 22:42:08 +0530

    added paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -73,6 +73,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :cover)
     end
 end
