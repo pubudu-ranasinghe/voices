b59094b34950917ae54905923be9f7031c83a28e
Pubudu <pubudutr@gmail.com>
2015-11-01 06:59:47 +0530

    dvjnwk

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031202301) do
+ActiveRecord::Schema.define(version: 20151031202450) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20151031202301) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fname"
+    t.string   "lname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
